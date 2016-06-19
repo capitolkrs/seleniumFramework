0ddc8f4d12d14692a5ba6db6f8df08297c80e1cf
capitol <capitolkrs@gmail.com>
2016-06-19 15:37:29 -0400

    Added BaseModel

---
+++ src/main/java/com/seleniumFramework/model/BaseModel.java
@@ -0,0 +1,11 @@
+package com.seleniumFramework.model;
+
+public class BaseModel {
+
+	public String runDataSource;
+	public String excelDataPath;
+	public boolean almTCColumnPresent;
+	public String excelDataSheet;
+	
+	
+}
