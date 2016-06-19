31f29494089ea9c3278783177b58bcd55911b691
capitol <capitolkrs@gmail.com>
2016-06-19 15:37:46 -0400

    Added DataTable

---
+++ src/main/java/com/seleniumFramework/helper/DataTable.java
@@ -0,0 +1,11 @@
+package com.seleniumFramework.helper;
+
+import java.util.Map;
+
+import com.seleniumFramework.model.*;
+
+public class DataTable {
+	public TestCase currentTestCase;
+
+	public Map<String, String>currentItdataMap;
+}
