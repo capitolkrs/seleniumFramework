443e7982d8bdfad95f64d7c733be9ba0730b60b2
capitol <capitolkrs@gmail.com>
2016-06-19 15:39:23 -0400

    Added slfConstants

---
+++ src/main/java/com/seleniumFramework/util/SLFMConstants.java
@@ -0,0 +1,13 @@
+package com.seleniumFramework.util;
+
+public class SLFMConstants {
+	
+	public static final String TEST_CASE_NAME="Test Case Name";
+	public static final String ITERATIONS="Iterations";
+	public static final Object XLSX = "xlsx";
+	public static final Object EXECUTE = "execute";
+	public static final Object ENVIRONMENT = "environment";
+	public static final String COMMON_DATA = "common data";
+	public static final String ENVIRONMENT_URL = "environment url";
+
+}
