30cc7496242a7b90b360bd466a019033ed49fd1b
capitol <capitolkrs@gmail.com>
2016-06-26 23:23:15 -0400

    update to excel util

--- src/main/java/com/seleniumFramework/util/ExcelUtil.java
+++ src/main/java/com/seleniumFramework/util/ExcelUtil.java
@@ -1,7 +1,11 @@
 package com.seleniumFramework.util;
 
 public class ExcelUtil {
-
+/**
+ * get excel file extension
+ * @param fileName
+ * @return
+ */
 	public static String getExcelFileExtn(String fileName) {
 		String excelExtn=null;
 		excelExtn=fileName.substring(fileName.indexOf("."),fileName.length());
